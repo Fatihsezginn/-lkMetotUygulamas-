768d8c8fc8759d81ec2db0f189c8b46768258bbb
pc <pc@DESKTOP-78M62IA>
2024-08-05 19:09:33 +0300

    Add project files.

---
+++ İlkMetotUygulaması/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void YazdirSarkiSozu()
+    {
+        Console.WriteLine("Uzun İnce Bir Yoldayım");
+    }
+
+    static int RastgeleSayiKalan()
+    {
+        Random rastgele = new Random();
+        return rastgele.Next() % 2;
+    }
+
+    static int Carpma(int a, int b)
+    {
+        return a * b;
+    }
+
+    static void Hosgeldiniz(string isim, string soyisim)
+    {
+        Console.WriteLine($"Hoş Geldiniz {isim} {soyisim}");
+    }
+
+    static void Main(string[] args)
+    {
+        YazdirSarkiSozu();
+        Console.WriteLine($"Rastgele sayının 2'ye bölümünden kalan: {RastgeleSayiKalan()}");
+        Console.WriteLine($"3 ve 8'nin çarpımı: {Carpma(3, 8)}");
+        Hosgeldiniz("Fatih", "Sezgin");
+    }
+}
